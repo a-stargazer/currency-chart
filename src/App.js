8ab73a29f28fc380f4fde62548f493e2b1822bca
Alice <aliceyang013@gmail.com>
2020-06-10 13:49:23 -0700

    added functioning dropdown bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [currencies, setCurrencies] = useState([
+    "CAD", "AUD", "HKD", "USD", "GBP",
+    ]);
+  const [rates, setRates] = useState([]);
+  const [baseCurrency, setBaseCurrency] = useState('USD');
+
+  function doFetch() {
+      const url = "https://api.exchangeratesapi.io/latest?base=" + baseCurrency;
+      fetch(url)
+          .then(response => response.json())
+          .then(data => {
+              console.log('data rates:', data.rates);
+              setRates(Object.entries(data.rates));
+      });
+  }
+
+  function onSelect(event) {
+    setBaseCurrency(event.target.value)
+  }
+
+  useEffect(doFetch, [baseCurrency]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="NavBar">
+      <h1>Currency Rates</h1>
+      <label>Choose a currency:
+        <select onChange={onSelect} value={baseCurrency}>
+          {
+            rates.map(rate => (
+              <option value={rate[0]}>{rate[0]}</option>
+            ))
+          }
+        </select> 
+      </label>
+
+    <div className="BarChart">
+    {
+      rates
+      .filter(rate => currencies.includes(rate[0])) 
+      .map(rate => (
+        <div 
+        className="BarChart-bar" 
+        style={{height: (1/rate[1] * 80) + "%"}}>
+         {rate[0]}<br></br>
+         {rate[1]}
+        </div>
+      ))
+    }
     </div>
+  </div>
   );
 }
 
